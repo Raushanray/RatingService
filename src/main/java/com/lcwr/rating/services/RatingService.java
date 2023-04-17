405eaecc3e5a9cceec5509319411e6d5136a7090
Raushanray <raushankuray1998@gmail.com>
2023-04-17 11:15:12 +0530

    ceate Rating_Service Project inside this project i am crating multiple APIs like create Rating, get All Rating, get single Rating with the help of userId, and get single Rating with the help of hotelId and also handle the runtime exceptions .... and for storing the data i am using mongodb database...

---
+++ src/main/java/com/lcwr/rating/services/RatingService.java
@@ -0,0 +1,22 @@
+package com.lcwr.rating.services;
+
+import com.lcwr.rating.entities.Rating;
+
+import java.util.List;
+
+public interface RatingService {
+
+    //create
+    Rating create(Rating rating);
+
+    //get all ratings
+    List<Rating> getRating();
+
+
+    //get all by userId
+    List<Rating> getRatingByUserId(String userId);
+
+    //get all by hotelId
+    List<Rating> getRatingByHotelId(String hotelId);
+
+}
